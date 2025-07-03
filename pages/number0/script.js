e589121487f377cec395481aea4c237b464d02a4
David Ian Leidi <121301645+dazzling-nova@users.noreply.github.com>
2025-07-03 15:12:07 +0200

    Add files via upload

---
+++ pages/number0/script.js
@@ -0,0 +1,23 @@
+const numberArticles = document.querySelector(".number-articles");
+
+// acquire articles from json file and populate the number's articles
+
+fetch("_articles.json")
+  .then((response) => response.json())
+  .then((json) => {
+    json.articles.forEach((element) => {
+      let author = element.autore;
+      let text = element.testo;
+      let title = element.titolo;
+
+      numberArticles.innerHTML += `<a href="${title}.html">
+            <article class="article-card">
+                <p class="article-title">${title}</p>
+                <p class="article-card-text">${text
+                  .substring(0, 200)
+                  .replace("<p>", "")
+                  .replace("</p>", "")}...</p>
+            </article>
+        </a>`;
+    });
+  });
